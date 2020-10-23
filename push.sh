12c0dca839ad51750ef0cc62a89b390cec6580b4
hz2 <5412693+JensenHua@user.noreply.gitee.com>
2020-10-23 17:07:40 +0800

    2020年10月23日 17:07:40+update

--- push.sh
+++ push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # push更新到仓库
-MESSAGE=`date`
+MESSAGE=`date`+"update"
 echo $MESSAGE
 
 echo "Adding files to git"
 git add .
 
+echo "Removing useless files and dirs"
+git rm push.sh
 git rm -r .idea/
 echo "Adding message"
 git commit -m "$MESSAGE"
